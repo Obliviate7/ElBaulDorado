a97ebbadaea6e986205162e5d00712f94167b84f
fabiancrux <fabiancrux@hotmail.com>
2017-10-08 11:36:36 -0300

    navAparte
    navAparte

--- mailSend.php
+++ mailSend.php
@@ -8,63 +8,7 @@
       header('Location: index.php');
     }
 ?>
-<!DOCTYPE html>
-<html>
-  <head>
-    <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1, user-scalable=no">
-    <meta charset="utf-8">
-    <link href="https://fonts.googleapis.com/css?family=Lora:400,700|Playfair+Display:i" rel="stylesheet">
-    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
-    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.1.1/jquery.min.js"></script>
-    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
-    <link rel="stylesheet" href="css/style.css">
-    <title>El Baul Dorado - Mail Enviado</title>
-  </head>
-  <body>
-    <header class="mainHeader">
-        <a class="title" href="index.php">El Baul Dorado</a>
-    </header>
-
-    <nav class="mainNav navbar navbar-default">
-      <div class="container-fluid">
-        <button class="navbar-toggle" type="button" data-toggle="collapse" data-target=".navbar-collapse">
-          <span class="icon-bar"></span>
-          <span class="icon-bar"></span>
-          <span class="icon-bar"></span>
-        </button>
-        <div class="collapse navbar-collapse" id="myNavbar" roll="navigation">
-          <ul class="nav navbar-nav">
-            <li><a href="#">Mujeres</a></li>
-            <li><a href="#">Hombres</a></li>
-            <li><a href="#">Conocenos</a></li>
-            <li><a href="faq.php">FAQ's</a></li>
-          </ul>
-          <ul class="nav navbar-nav navbar-right">
-            <span>
-            <li>
-               <?php
-               if (isset($_SESSION['email'])) {
-                    echo '<a class="logButtons" href="logOut.php"><span class="logButtons glyphicon glyphicon-user"></span>Deslogear</a>';
-               } else {
-                  echo '<a class="logButtons" href="register.php"><span class="logButtons glyphicon glyphicon-user"></span>Registrate</a>';
-                  }
-               ?>
-              </li>
-          </span>
-          <span><li>
-              <?php
-              if (isset($_SESSION['email'])) {
-                 echo $_SESSION['email'];
-              } else {
-                 echo'<a class="logButtons" href="login.php" ><span class="logButtons glyphicon glyphicon-log-in"></span>Login</a>';
-                 }
-              ?>
-          </li>
-          </span>
-          </ul>
-        </div>
-      </div>
-    </nav>
+<?php include("header.php"); ?>
     <div class="containerLogin">
       <h2 class="titleLogin" >Correo enviado</h2>
       <form>
--- profile.php
+++ profile.php
@@ -20,64 +20,7 @@ if ($completed) {
       }
  }
  ?>
-<!DOCTYPE html>
-<html>
-  <head>
-    <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1, user-scalable=no">
-    <meta charset="utf-8">
-    <link href="https://fonts.googleapis.com/css?family=Lora:400,700|Playfair+Display:i" rel="stylesheet">
-    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
-    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.1.1/jquery.min.js"></script>
-    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
-    <link rel="stylesheet" href="css/style.css">
-    <title class="titleLink">El Baul Dorado - Profile</title>
-  </head>
-  <body>
-    <header class="mainHeader">
-        <a class="title" href="index.php">El Baul Dorado</a>
-    </header>
-        <nav class="mainNav navbar navbar-default">
-          <div class="container-fluid">
-            <button class="navbar-toggle" type="button" data-toggle="collapse" data-target=".navbar-collapse">
-              <span class="icon-bar"></span>
-              <span class="icon-bar"></span>
-              <span class="icon-bar"></span>
-            </button>
-            <div class="collapse navbar-collapse" id="myNavbar" roll="navigation">
-              <ul class="nav navbar-nav">
-                <li><a href="#">Mujeres</a></li>
-                <li><a href="#">Hombres</a></li>
-                <li><a href="#">Conocenos</a></li>
-                <li><a href="faq.php">FAQ's</a></li>
-              </ul>
-              <ul class="nav navbar-nav navbar-right">
-                <span>
-                <li>
-                   <?php
-                   /*si esta iniciada la sesion muestra el deslogear, si no el register*/
-                   if (isset($_SESSION['email'])) {
-                        echo '<a class="logButtons " href="logOut.php"><span class="logButtons glyphicon glyphicon-user"></span>Deslogear</a>';
-                   } else {
-                      echo '<a class="logButtons" href="register.php"><span class="logButtons glyphicon glyphicon-user"></span>Registrate</a>';
-                      }
-                   ?>
-                  </li>
-              </span>
-              <span><li>
-                  <?php
-                  /*si esta iniciada la sesion muestra el mail, si no el login*/
-                  if (isset($_SESSION['email'])) {
-                     echo $_SESSION['usrName'];
-                  } else {
-                     echo'<a class="logButtons" href="login.php" ><span class="logButtons glyphicon glyphicon-log-in"></span>Login</a>';
-                     }
-                  ?>
-              </li>
-              </span>
-              </ul>
-            </div>
-          </div>
-        </nav>
+<?php include("header.php"); ?>
     <div class="containerRegister">
       <h2 class="titleRegister">Modifica tus datos de perfil</h2>
       <form action="register.php" method="post" enctype="multipart/form-data" class="formRegister">
